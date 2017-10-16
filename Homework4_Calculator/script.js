156771c3dccf7e0d7b335ef8ded8119b58b12f53
Palette25 <30751070+Palette25@users.noreply.github.com>
2017-10-16 22:32:02 +0800

    homework4 update

--- Homework4_Calculator/script.js
+++ Homework4_Calculator/script.js
@@ -1,5 +1,11 @@
 var input = document.getElementById("input");
 
+var temp = document.getElementsByTagName("button");
+
+for(var i=0;i<temp.length;i++) {
+    temp[i].addEventListener("click", function(){add(this.id);});
+}
+
 function add(temp){
     if(temp==="CE") input.value = "";
     else if(temp==="arrow"){
@@ -17,11 +23,24 @@ function add(temp){
 
 function getResult(text){
     try{
+        for(var i=0;i<text.length;i++){
+            if((text[i]>='0'&&text[i]<='9')|| text[i]==='+'
+                ||text[i]==='-'|| text[i]==='*'||text[i]==='/'
+                ||text[i]==='('||text[i]===')'|| text[i]==='.') continue;
+            else throw "Error";
+        }
         var result = eval(text);
-        if(result===Infinity) throw "Error";
-        else alert("该运算式的结果是: " + eval(text));
+        if(result!==Infinity||flag===false) alert("该运算式的结果是: " + eval(text));
+        else throw "Error";
     }catch(error){
         alert("该表达式是非法的!");
         input.value = "";
+    }
+}
+
+function Enter() {
+    if(window.event.keyCode===13){
+        var text = input.value;
+        getResult(text);
     }
 }
